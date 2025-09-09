1359e9d483bf3ee5999bafb9a2ceb8171f4d56c1
Olexii Yermolenko <AlexeyErmolenko@i.ua>
2025-09-09 14:07:25 +0300

    Fix MONGODB_DB

--- server/src/db/initMongoConnection.js
+++ server/src/db/initMongoConnection.js
@@ -9,7 +9,7 @@ const initMongoConnection = async () => {
     const db = getEnvVar('MONGODB_DB');
 
     await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=FlowerDelivery`,
+      `mongodb+srv://${user}:${pwd}@${url}/?retryWrites=true&w=majority&appName=${db}`,
     );
     console.log('MongoDB connection successfully established!');
   } catch (error) {
